05d2f2d68aa6ae6cc53d02143938aad0817232b0
alex <alexalpek@gmail.com>
2020-06-11 23:34:10 +0200

    another part of phonebook completed

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 import Title from "./components/Title";
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-123456" },
-        { name: "Ada Lovelace", number: "39-44-5323523" },
-        { name: "Dan Abramov", number: "12-43-234345" },
-        { name: "Mary Poppendieck", number: "39-23-6423122" },
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [newPhoneNumber, setNewPhoneNumber] = useState("");
     const [filterWord, setFilterWord] = useState("");
     const [filtered, setFiltered] = useState(false);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/persons").then((response) => {
+            setPersons(response.data);
+        });
+    }, []);
 
     return (
         <div>
